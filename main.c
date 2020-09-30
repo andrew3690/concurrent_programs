f7e5942a535e1c3a5c6105add8b61a2e374aa2c6
andrew3690 <42048231+andrew3690@users.noreply.github.com>
2020-09-30 11:47:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,51 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
-//       (pai)      
-//         |        
-//    +----+----+
-//    |         |   
-// filho_1   filho_2
 
+//                          (principal)
+//                               |
+//              +----------------+--------------+
+//              |                               |
+//           filho_1                         filho_2
+//              |                               |
+//    +---------+-----------+          +--------+--------+
+//    |         |           |          |        |        |
+// neto_1_1  neto_1_2  neto_1_3     neto_2_1 neto_2_2 neto_2_3
 
 // ~~~ printfs  ~~~
-// pai (ao criar filho): "Processo pai criou %d\n"
-//    pai (ao terminar): "Processo pai finalizado!\n"
-//  filhos (ao iniciar): "Processo filho %d criado\n"
+//      principal (ao finalizar): "Processo principal %d finalizado\n"
+// filhos e netos (ao finalizar): "Processo %d finalizado\n"
+//    filhos e netos (ao inciar): "Processo %d, filho de %d\n"
 
 // Obs:
-// - pai deve esperar pelos filhos antes de terminar!
+// - netos devem esperar 5 segundos antes de imprmir a mensagem de finalizado (e terminar)
+// - pais devem esperar pelos seu descendentes diretos antes de terminar
 
-
+//Meu codigo que vale 6 pontos 
+/*pid_t filho,neto;
+		filho = fork();
+		if (filho < 0){
+			fprintf(stderr, "Erro ao criar filho\n");
+		}else if(filho == 0){
+			printf("Processo %d filho de %d\n",getpid(),getppid());
+			for(int i = 0 ;i < 3;i++){
+				neto = fork();
+				if(neto < 0){
+					fprintf(stderr, "Erro ao criar o neto\n");
+				} else if(neto == 0){
+					printf("Processo %d filho de %d\n",getpid(),getppid()); 
+				} else {
+					wait(NULL);
+					printf("Processo %d finalizado\n",getpid());
+				}
+			}
+		}else {
+			wait(NULL);
+			printf("Processo principal %d finalizado\n", getpid());  
+		}
+*/
 int main(int argc, char** argv) {
-
+    
     // ....
 
     /*************************************************
@@ -30,17 +57,41 @@ int main(int argc, char** argv) {
      * 2. Faça os prints exatamente como solicitado. *
      * 3. Espere o término dos filhos                *
      *************************************************/
-	for(int i=0;i<2;i++)
-    { 
-        if(fork() == 0) 
+	pid_t filho,neto; // declaraçao de processos filhos e netos
+	for(int i=0;i<2;i++) // loop para criaçao dos processos filhos
+    	{ 
+        if((filho = fork()) == 0) // criaçao do processo fiho
         { 
-        printf("O processo pai criou %d\n",getpid());
-        printf("O processo filho %d foi criado\n",getpid());
+        printf("Processo %d, filho de %d\n",getpid(),getppid());
+        fflush(stdout); // evita repetiçao de print
+        for(int i = 0;i < 3;i++){ // loop pra criaçao do processo neto
+        	if ((neto =fork()) == 0){ // criaçao e execuçao do processo neto
+        	printf("Processo %d filho de %d\n",getpid(),getppid());
+        	sleep(5);
+        	printf("Processo %d finalizado\n",getpid());
+        	exit(0);
+        	}
+        }
+        for(int i = 0; i < 3;i++){ // loop para espera da finalizaçao de cada processo neto 
+        	wait(NULL);
+        }
+        printf("Processo %d finalizado\n",getpid()); // finalizaçao do processo
         exit(0);
         }
-    } 
-    for(int i=0;i<2;i++){ 
-        wait(NULL);
-    }
-    printf("Processo pai finalizado!\n");
-}
+	} 
+	    for(int i=0;i<2;i++){ // loop para espera da finalizaçao de cada processo filho
+		wait(NULL);
+	    }
+		printf("Processo principal %d finalizado\n", getpid()); // finalizaçao de processo filho
+	    }
+
+		printf("O processo pai criou %d\n",getpid());
+		printf("O processo filho %d foi criado\n",getpid());
+		exit(0);
+		}
+	    } 
+	    for(int i=0;i<2;i++){ // loop para espera da finalizaçao do processo filho 
+		wait(NULL);
+	    }
+	    printf("Processo pai finalizado!\n");
+	}
